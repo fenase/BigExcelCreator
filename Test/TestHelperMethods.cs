02a6920c7df8e5d654134ae045f1d531f5edb7bb
Federico Seckel <fenase@outlook.com>
2022-10-15 02:33:43 -0300

    removed usings

--- Test/TestHelperMethods.cs
+++ Test/TestHelperMethods.cs
@@ -1,11 +1,6 @@
 ﻿using BigExcelCreator;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test
 {
