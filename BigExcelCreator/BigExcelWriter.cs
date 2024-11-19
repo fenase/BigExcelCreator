705843ed634c4559841b02ebb3232d247f0c7467
Federico Seckel <fenase@outlook.com>
2024-11-18 23:21:34 -0300

    Add decimal data validation methods to BigExcelCreator
    Introduce new methods in BigExcelWriter.cs to add decimal data validation to specified cell ranges. Methods support both string and CellRange inputs, with options for float and double operands. Detailed XML documentation comments are included. Internal helper method AddValidatorCommon and AppendNewDataValidation are used for setting up and applying validation rules. Methods include checks for required operands and throw exceptions if necessary.

--- BigExcelCreator/BigExcelWriter.cs
+++ BigExcelCreator/BigExcelWriter.cs
@@ -1404,6 +1404,130 @@ namespace BigExcelCreator
         }
 
         /// <summary>
+        /// Adds a decimal data validation to the specified cell range.
+        /// </summary>
+        /// <param name="range">The cell range to apply the validation to.</param>
+        /// <param name="firstOperand">The first operand for the validation.</param>
+        /// <param name="validationType">The type of validation to apply.</param>
+        /// <param name="allowBlank">If set to <c>true</c>, blank values are allowed.</param>
+        /// <param name="showInputMessage">If set to <c>true</c>, an input message will be shown.</param>
+        /// <param name="showErrorMessage">If set to <c>true</c>, an error message will be shown when invalid data is entered.</param>
+        /// <param name="secondOperand">The second operand for the validation, if required by the validation type.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the validation type requires a second operand but <paramref name="secondOperand"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="range"/> is <c>null</c>.</exception>
+        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the validation to.</exception>
+        /// <exception cref="InvalidRangeException">Thrown when the <paramref name="range"/> does not represent a valid range.</exception>
+        public void AddDecimalValidator(string range,
+                                        float firstOperand,
+                                        DataValidationOperatorValues validationType,
+                                        bool allowBlank = true,
+                                        bool showInputMessage = true,
+                                        bool showErrorMessage = true,
+                                        float? secondOperand = null)
+        {
+            AddDecimalValidator(new CellRange(range),
+                                firstOperand,
+                                validationType,
+                                allowBlank,
+                                showInputMessage,
+                                showErrorMessage,
+                                secondOperand);
+        }
+
+        /// <summary>
+        /// Adds a decimal data validation to the specified cell range.
+        /// </summary>
+        /// <param name="range">The cell range to apply the validation to.</param>
+        /// <param name="firstOperand">The first operand for the validation.</param>
+        /// <param name="validationType">The type of validation to apply.</param>
+        /// <param name="allowBlank">If set to <c>true</c>, blank values are allowed.</param>
+        /// <param name="showInputMessage">If set to <c>true</c>, an input message will be shown.</param>
+        /// <param name="showErrorMessage">If set to <c>true</c>, an error message will be shown when invalid data is entered.</param>
+        /// <param name="secondOperand">The second operand for the validation, if required by the validation type.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the validation type requires a second operand but <paramref name="secondOperand"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="range"/> is <c>null</c>.</exception>
+        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the validation to.</exception>
+        public void AddDecimalValidator(CellRange range,
+                                        float firstOperand,
+                                        DataValidationOperatorValues validationType,
+                                        bool allowBlank = true,
+                                        bool showInputMessage = true,
+                                        bool showErrorMessage = true,
+                                        float? secondOperand = null)
+        {
+            DataValidation dataValidation = AddValidatorCommon(range, DataValidationValues.Decimal, validationType, allowBlank, showInputMessage, showErrorMessage);
+
+            if (validationType.RequiresSecondOperand() && secondOperand == null)
+            {
+                throw new ArgumentNullException(nameof(secondOperand), $"validation type {validationType} requires a second operand");
+            }
+
+            AppendNewDataValidation(dataValidation, firstOperand.ToString(CultureInfo.InvariantCulture), secondOperand?.ToString(CultureInfo.InvariantCulture));
+        }
+
+        /// <summary>
+        /// Adds a decimal data validation to the specified cell range.
+        /// </summary>
+        /// <param name="range">The cell range to apply the validation to.</param>
+        /// <param name="firstOperand">The first operand for the validation.</param>
+        /// <param name="validationType">The type of validation to apply.</param>
+        /// <param name="allowBlank">If set to <c>true</c>, blank values are allowed.</param>
+        /// <param name="showInputMessage">If set to <c>true</c>, an input message will be shown.</param>
+        /// <param name="showErrorMessage">If set to <c>true</c>, an error message will be shown when invalid data is entered.</param>
+        /// <param name="secondOperand">The second operand for the validation, if required by the validation type.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the validation type requires a second operand but <paramref name="secondOperand"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="range"/> is <c>null</c>.</exception>
+        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the validation to.</exception>
+        /// <exception cref="InvalidRangeException">Thrown when the <paramref name="range"/> does not represent a valid range.</exception>
+        public void AddDecimalValidator(string range,
+                                        double firstOperand,
+                                        DataValidationOperatorValues validationType,
+                                        bool allowBlank = true,
+                                        bool showInputMessage = true,
+                                        bool showErrorMessage = true,
+                                        double? secondOperand = null)
+        {
+            AddDecimalValidator(new CellRange(range),
+                                firstOperand,
+                                validationType,
+                                allowBlank,
+                                showInputMessage,
+                                showErrorMessage,
+                                secondOperand);
+        }
+
+        /// <summary>
+        /// Adds a decimal data validation to the specified cell range.
+        /// </summary>
+        /// <param name="range">The cell range to apply the validation to.</param>
+        /// <param name="firstOperand">The first operand for the validation.</param>
+        /// <param name="validationType">The type of validation to apply.</param>
+        /// <param name="allowBlank">If set to <c>true</c>, blank values are allowed.</param>
+        /// <param name="showInputMessage">If set to <c>true</c>, an input message will be shown.</param>
+        /// <param name="showErrorMessage">If set to <c>true</c>, an error message will be shown when invalid data is entered.</param>
+        /// <param name="secondOperand">The second operand for the validation, if required by the validation type.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the validation type requires a second operand but <paramref name="secondOperand"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="range"/> is <c>null</c>.</exception>
+        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the validation to.</exception>
+        public void AddDecimalValidator(CellRange range,
+                                        double firstOperand,
+                                        DataValidationOperatorValues validationType,
+                                        bool allowBlank = true,
+                                        bool showInputMessage = true,
+                                        bool showErrorMessage = true,
+                                        double? secondOperand = null)
+        {
+            DataValidation dataValidation = AddValidatorCommon(range, DataValidationValues.Decimal, validationType, allowBlank, showInputMessage, showErrorMessage);
+
+            if (validationType.RequiresSecondOperand() && secondOperand == null)
+            {
+                throw new ArgumentNullException(nameof(secondOperand), $"validation type {validationType} requires a second operand");
+            }
+
+            AppendNewDataValidation(dataValidation, firstOperand.ToString(CultureInfo.InvariantCulture), secondOperand?.ToString(CultureInfo.InvariantCulture));
+        }
+
+        /// <summary>
         /// Adds a comment to a specified cell range.
         /// </summary>
         /// <param name="text">The text of the comment.</param>
