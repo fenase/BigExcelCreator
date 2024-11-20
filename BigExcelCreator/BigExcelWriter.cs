a8effe95af94f47a31aa5f3b40921d442d7cb753
Federico Seckel <fenase@outlook.com>
2024-11-20 19:00:18 -0300

    Update ignored spellings in BigExcelWriter.cs
    Removed "rownum" and "Validator" from the list of ignored spellings in the comment. This change ensures that the spelling of these terms will now be checked.

--- BigExcelCreator/BigExcelWriter.cs
+++ BigExcelCreator/BigExcelWriter.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2022-2025, Federico Seckel.
 // Licensed under the BSD 3-Clause License. See LICENSE file in the project root for full license information.
 
-// Ignore Spelling: rownum Validator Validators Autofilter stylesheet finalizer inline unhiding gridlines
+// Ignore Spelling: Validators Autofilter stylesheet finalizer inline unhiding gridlines
 
 using BigExcelCreator.CommentsManager;
 using BigExcelCreator.Enum;
