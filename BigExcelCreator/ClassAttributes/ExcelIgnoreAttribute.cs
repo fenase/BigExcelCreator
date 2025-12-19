0e72a4965716742b3dd88d762e6e22d73dcd2570
Federico Seckel <fenase@outlook.com>
2025-12-19 18:38:23 -0300

    Add Excel export attribute classes and CellType enum
    - Introduce attribute classes for Excel export customization:
      - ExcelColumnHiddenAttribute
      - ExcelColumnNameAttribute
      - ExcelColumnOrderAttribute
      - ExcelColumnTypeAttribute
      - ExcelColumnWidthAttribute
      - ExcelIgnoreAttribute
    - Add CellType enum for specifying cell types.
    
    Attributes are sealed, property-targeted, and use modern C# features.
    Includes XML documentation and license headers.

---
+++ BigExcelCreator/ClassAttributes/ExcelIgnoreAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2022-2025, Federico Seckel.
+// Licensed under the BSD 3-Clause License. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace BigExcelCreator.ClassAttributes
+{
+    /// <summary>
+    /// Marks a property to be ignored when generating Excel output.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class ExcelIgnoreAttribute : Attribute
+    {
+    }
+}
