490126319009fb2319337beabf0c776a36dcd64e
Federico Seckel <fenase@outlook.com>
2025-12-23 23:03:25 -0300

    comments

--- BigExcelCreator/BigExcelWriter.cs
+++ BigExcelCreator/BigExcelWriter.cs
@@ -4,7 +4,6 @@
 // Ignore Spelling: Validator Validators Autofilter stylesheet finalizer inline unhiding gridlines rownum
 
 using BigExcelCreator.ClassAttributes;
-using BigExcelCreator.CommentsManager;
 using BigExcelCreator.Enums;
 using BigExcelCreator.Exceptions;
 using BigExcelCreator.Extensions;
@@ -230,51 +229,6 @@ namespace BigExcelCreator
             maxColumnNum = Math.Max(columnNum - 1, maxColumnNum);
             columnNum = 1;
             rowOpen = false;
-        }
-
-        /// <summary>
-        /// Adds a comment to a specified cell range.
-        /// </summary>
-        /// <param name="text">The text of the comment.</param>
-        /// <param name="reference">The cell range where the comment will be added. Must be a single cell range.</param>
-        /// <param name="author">The author of the comment. Default is "BigExcelCreator".</param>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="author"/> is null or empty, or when <paramref name="reference"/> is not a single cell range.</exception>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="reference"/> is null.</exception>
-        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the comment to.</exception>
-        /// <exception cref="InvalidRangeException">Thrown when the <paramref name="reference"/> does not represent a valid range.</exception>
-        public void Comment(string text, string reference, string author = "BigExcelCreator")
-        {
-            CellRange cellRange = new(reference);
-            Comment(text, cellRange, author);
-        }
-
-        /// <summary>
-        /// Adds a comment to a specified cell range.
-        /// </summary>
-        /// <param name="text">The text of the comment.</param>
-        /// <param name="cellRange">The cell range where the comment will be added. Must be a single cell range.</param>
-        /// <param name="author">The author of the comment. Default is "BigExcelCreator".</param>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="author"/> is null or empty, or when <paramref name="cellRange"/> is not a single cell range.</exception>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="cellRange"/> is null.</exception>
-        /// <exception cref="NoOpenSheetException">Thrown when there is no open sheet to add the comment to.</exception>
-        public void Comment(string text, CellRange cellRange, string author = "BigExcelCreator")
-        {
-            if (string.IsNullOrEmpty(author)) { throw new ArgumentOutOfRangeException(nameof(author)); }
-#if NET6_0_OR_GREATER
-            ArgumentNullException.ThrowIfNull(cellRange);
-#else
-            if (cellRange == null) { throw new ArgumentNullException(nameof(cellRange)); }
-#endif
-            if (!cellRange.IsSingleCellRange) { throw new ArgumentOutOfRangeException(nameof(cellRange), string.Format(CultureInfo.InvariantCulture, ConstantsAndTexts.TwoWordsConcatenation, nameof(cellRange), ConstantsAndTexts.MustBeASingleCellRange)); }
-            if (!sheetOpen) { throw new NoOpenSheetException(string.Format(CultureInfo.InvariantCulture, ConstantsAndTexts.TwoWordsConcatenation, "Comments", ConstantsAndTexts.NeedToBePlacedOnSSheet)); }
-
-            commentManager ??= new();
-            commentManager.Add(new CommentReference()
-            {
-                Cell = cellRange.RangeStringNoSheetName,
-                Text = text,
-                Author = author,
-            });
         }
 
         /// <summary>
