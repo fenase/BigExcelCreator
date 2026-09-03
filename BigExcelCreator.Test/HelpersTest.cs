ad8e8c93052b11f1f5298037e4f3c05baf1a8957
Federico Seckel <fenase@outlook.com>
2026-09-03 14:40:34 -0300

    testing for `ConditionalFormattingOperatorValues Value()`

--- BigExcelCreator.Test/HelpersTest.cs
+++ BigExcelCreator.Test/HelpersTest.cs
@@ -1,4 +1,8 @@
-﻿namespace BigExcelCreator.Test
+﻿using BigExcelCreator.Enums;
+using BigExcelCreator.Extensions;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace BigExcelCreator.Test
 {
     internal class HelpersTest
     {
@@ -40,6 +44,29 @@
             {
                 Assert.That(Helpers.GetColumnIndex(Helpers.GetColumnName(i)), Is.EqualTo(i));
             }
+        }
+
+        [Test]
+        public void GetConditionalFormattingOperatorValuesValue()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(ConditionalFormattingOperator.LessThan.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.LessThan));
+                Assert.That(ConditionalFormattingOperator.LessThanOrEqual.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.LessThanOrEqual));
+                Assert.That(ConditionalFormattingOperator.Equal.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.Equal));
+                Assert.That(ConditionalFormattingOperator.NotEqual.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.NotEqual));
+                Assert.That(ConditionalFormattingOperator.GreaterThanOrEqual.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.GreaterThanOrEqual));
+                Assert.That(ConditionalFormattingOperator.GreaterThan.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.GreaterThan));
+                Assert.That(ConditionalFormattingOperator.Between.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.Between));
+                Assert.That(ConditionalFormattingOperator.NotBetween.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.NotBetween));
+                Assert.That(ConditionalFormattingOperator.ContainsText.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.ContainsText));
+                Assert.That(ConditionalFormattingOperator.NotContains.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.NotContains));
+                Assert.That(ConditionalFormattingOperator.BeginsWith.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.BeginsWith));
+                Assert.That(ConditionalFormattingOperator.EndsWith.Value(), Is.EqualTo(ConditionalFormattingOperatorValues.EndsWith));
+
+                Assert.Throws<ArgumentOutOfRangeException>(() => ((ConditionalFormattingOperator)999).Value());
+                Assert.Throws<ArgumentOutOfRangeException>(() => ((ConditionalFormattingOperator)(-1)).Value());
+            });
         }
     }
 }
